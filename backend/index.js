71a7f87f1eab18e158c0fbb1e97eb11c26d76c2a
Carissa Co <carissahvc@gmail.com>
2026-02-09 14:35:05 -0800

    create docker compose file

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const db = mysql.createConnection({
     host: process.env.DB_HOST || "localhost",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
+    password: process.env.DB_PASSWORD || "password",
     database: process.env.DB_NAME || "test"
 })
 
